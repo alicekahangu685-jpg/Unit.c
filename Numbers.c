d3c9fa5e2759e78dbfcedccd14141b56dbf95621
alicekahangu685-jpg <alicekahangu685@gmail.com>
2025-09-25 14:18:50 +0300

    Add files via upload

---
+++ Numbers.c
@@ -0,0 +1,23 @@
+//Simple C program 
+/*
+Name:Kahangu Alice Kabura 
+Reg No:CT101/G/26425/25
+Description:Assignment 
+*/
+# include <stdio.h>
+// variable declaration 
+int main () {
+    int height;
+    printf("Enter your height:");
+    scanf("%d",&height);
+    printf("Your height is %d",1.50);
+    
+    float balance;
+    printf("Enter your bank balance");
+    scanf("%d",&balance);
+    printf("Your bank balance is %d");
+return 0;
+}   
+    
+    
+    
---
+++ Units.c
@@ -0,0 +1,19 @@
+//Simple program 
+/*
+Name:Kahangu Alice Kabura
+Reg no:CT101/G/26425/25
+Description:Assignment 
+*/
+#include <stdio.h>
+//main function
+int main(){ 
+    printf("The following are my units this semester\n");
+    printf("Introduction to Programming and Problem solving\n");
+    printf("Introductionto Computer Architecture\n");
+    printf("Physics\n");
+    printf("Analogue Electronics\n");
+    printf("Mathematics for science\n");
+    printf("Fundamentals of ICT\n");
+    printf("Communication skills");
+return 0;
+}   
